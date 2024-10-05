504aa4abf5313fbbb4a98f5c02e93a3467335ac9
Lengalei <ruthlengalei@gmail.com>
2024-10-05 12:36:57 +0300

    updates

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,7 +1,17 @@
 
 
 const login = async(req,res)=>{
+    try {
+        const [email, password] = req.body
+const founduser = await findone({email:email})
 
+// validating the email
+if (!founduser){
+    throw new error ('User not found')
+}
+    } catch (error) {
+        
+    }
 }
 
 export {login}
