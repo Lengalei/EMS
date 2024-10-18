17599f7fd76c437a133f5bbb2214f21b5a868639
Lengalei <ruthlengalei@gmail.com>
2024-10-19 00:15:59 +0300

    wersd

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,14 @@
-import express from "express"
-import cors from"cors"
+import express from "express";
+import cors from "cors";
+import authRouter from "./Routes/auth.js";
+import connectToDatabase from "./db/db.js";
+const app = express();
 
-const app = express()
+connectToDatabase();
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRouter);
 
-app.use(cors())
-app.use(express.json())
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server listening at port ${process.env.PORT}`);
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at port ${process.env.PORT}`);
+});
