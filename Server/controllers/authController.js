2411659670314b2776921d3c9e63576affb7a985
Lengalei <ruthlengalei@gmail.com>
2024-10-18 12:55:04 +0300

    updates

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,16 +1,20 @@
+import User from "../models/userModel.js"
+import bycrypt from 'bcrypt'
 
 
 const login = async(req,res)=>{
     try {
         const [email, password] = req.body
-const founduser = await findone({email:email})
+const foundUser = await User.findone({email:email})
 
 // validating the email
-if (!founduser){
+if (!foundUser){
     throw new error ('User not found')
 }
+
+const isValid = bycrypt.compare(password, foundUser.password)
     } catch (error) {
-        
+        res.status(500).json({message: 'Internal server error'})
     }
 }
 
