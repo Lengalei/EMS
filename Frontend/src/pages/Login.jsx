dc9d18f4dcad9b2c05be86d984d3432b0656eb61
Lengalei <ruthlengalei@gmail.com>
2024-10-19 00:15:48 +0300

    wesdds

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,45 +1,69 @@
 import React, { useState } from "react";
-import axios from 'axios'
+import axios from "axios";
+// import { TailSpin } from "react-loader-spinner";
 import "./Pages.scss";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setloading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const handlesubmit = async(e)=>{
-    e.preventDefault()
-            try {
-                const response = await axios.post("https://localhost:3000/api/auth/login", {email,password});
-            } catch (error) {
-                console.log(error)
-            }
-    
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/login",
+        {
+          email,
+          password,
+        }
+      );
+      setloading(false);
+
+      if (response.data.success) {
+        alert("Login successful");
+      }
+    } catch (error) {
+      if (error.response && !error.response.data.success) {
+        setError(error.response.data.error);
+      } else {
+        setError("server error" || message.error);
+      }
+    }
+  };
+
+  // if (loading) {
+  //   return <TailSpin color="#00BFFF" height={80} width={80} />;
+  // }
+
   return (
     <div className="login">
       <h2>Employee Management system Login</h2>
 
-      <form className="login-container" onSubmit={handlesubmit}>
+      <form className="login-container" onSubmit={handleSubmit}>
         <h3>LOGIN</h3>
-
+        {error && <p className="error">{error}</p>}
         <div className="input">
-          <label htmlFor="email">Email</label>
+          <label>Email</label>
           <input
             type="email"
             placeholder="Enter your email"
+            value={email}
             onChange={(e) => {
-              setEmail;
+              setEmail(e.target.value);
             }}
           />
         </div>
 
         <div className="input">
-          <label htmlFor="password">Password</label>
+          <label>Password</label>
           <input
             type="password"
             placeholder="****"
+            value={password}
             onChange={(e) => {
-              setPassword;
+              setPassword(e.target.value);
             }}
           />
         </div>
