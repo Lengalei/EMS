a8715b157263b1aa360abf77d0df8537c1c67546
Lengalei <ruthlengalei@gmail.com>
2024-11-14 11:57:54 +0300

    updates

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,19 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import authRouter from "./Routes/auth.js";
 import departmentRouter from "./Routes/department.routes.js";
 import employeeRouter from "./Routes/Employee.routes.js";
 import connectToDatabase from "./db/db.js";
 const app = express();
 
+dotenv.config();
 connectToDatabase();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRouter);
-app.use("/api", departmentRouter);
-app.use("/api", employeeRouter);
+app.use("/api/department", departmentRouter);
+app.use("/api/employee", employeeRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening at port ${process.env.PORT}`);
