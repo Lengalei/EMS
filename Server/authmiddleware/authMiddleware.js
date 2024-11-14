f7c84218a9bd6dcae1ebfe1605502d4078be13c4
Lengalei <ruthlengalei@gmail.com>
2024-11-14 11:56:40 +0300

    updates

--- Server/authmiddleware/authMiddleware.js
+++ Server/authmiddleware/authMiddleware.js
@@ -11,25 +11,21 @@ const verifyUser = async (req, res, next) => {
     }
 
     const decoded = jwt.verify(token, process.env.JWT_KEY);
-    if(!decoded){
-        return res
-          .status(404)
-          .json({ success: false, error: "Token not valid" });
+    if (!decoded) {
+      return res.status(404).json({ success: false, error: "Token not valid" });
     }
 
-    const user = await User.findById({_id: decoded._id}).select("-password")
+    const user = await User.findById({ _id: decoded._id }).select("-password");
 
     if (!user) {
       return res.status(404).json({ success: false, error: "user not found" });
     }
 
-     req.user = user;
-     next();
+    req.user = user;
+    next();
   } catch (error) {
     return res.status(500).json({ success: false, error: "Server side error" });
   }
-
- 
 };
 
 export default verifyUser;
--- Server/db/db.js
+++ Server/db/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const connectToDatabase = async (req, res) => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
+    console.log('server connected DB successfully')
   } catch (error) {
     console.log(error);
   }
