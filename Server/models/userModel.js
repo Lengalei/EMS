35d3337bb542d7442aa54df273209c89a5471d9c
Lengalei <ruthlengalei@gmail.com>
2024-11-18 15:26:01 +0300

    updates

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["Admin", "Employee"], required: true },
-  password: { type: String },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["Admin", "Employee"], required: true },
+    password: { type: String },
+    dob: { type: Date, default: Date.now },
+    department: { type: String },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
