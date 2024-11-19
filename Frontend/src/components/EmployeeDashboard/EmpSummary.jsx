588b9119e6d33dc7cdd48f61bfde4c490e0ea46b
Lengalei <ruthlengalei@gmail.com>
2024-11-19 12:54:58 +0300

    emp dashboard

---
+++ Frontend/src/components/EmployeeDashboard/EmpSummary.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function EmpSummary({}) {
+  return (
+    <div className="mainCard">
+      <div className="dashicon"></div>
+
+      <div className="dashCardContent">
+        <p>Welcome Back</p>
+        <p>Ruth</p>
+      </div>
+    </div>
+  );
+}
+
+export default EmpSummary;
