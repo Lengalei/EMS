5d85a4618202e47233f70857ca9510c5737bbc48
Lengalei <ruthlengalei@gmail.com>
2024-11-24 00:22:02 +0300

    redux

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDom from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthContextProvider } from "./context/authContext.jsx";
+import { Provider } from "react-redux";
+import { store } from "./App/Store.jsx";
 
 ReactDom.createRoot(document.getElementById("root")).render(
-  
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <Provider store={store}>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </Provider>
   </React.StrictMode>
 );
